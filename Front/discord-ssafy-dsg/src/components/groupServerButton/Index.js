8613518e8cb02438bb14008a3ea6a0fdf385a28f
jodie9596@gmail.com <jodie9596@gmail.com>
2023-02-04 22:05:59 +0900

    groupServerButton 컴포넌트 퍼블리싱

---
+++ Front/discord-ssafy-dsg/src/components/groupServerButton/Index.js
@@ -0,0 +1,19 @@
+import style from './groupServerButton.module.scss'
+import discordImg from '../../public/discord.png'
+
+function GroupServerButton( props ) {
+  return (
+    <div className={ `${style['group-server-button']}` }>
+      <div className={ `${style['left-selected-wrapper']}` }>
+        <div className={ `${style['left-selected']}` }></div>
+      </div>
+      <div className={ `${style['img-area']}` }>
+        <div className={ `${style['img-wrapper']}` }>
+          <img className={ `${style['img']}` } src={ discordImg } alt='discord-img'/>
+        </div>
+      </div>
+    </div>
+   )
+}
+
+export default GroupServerButton
--- Front/discord-ssafy-dsg/src/components/leftSideBar/Index.js
+++ Front/discord-ssafy-dsg/src/components/leftSideBar/Index.js
@@ -1,14 +1,11 @@
 import style from './leftSideBar.module.scss'
-import discordImg from '../../public/discord.png'
+import GroupServerButton from '../groupServerButton/Index'
 
 function LeftSideBar() {
   return (
    <div className={ `${style['left-side-bar']}` }>
     <div className={ `${style['first-area']}` }>
-      <div className={ `${style['img-area']}` }>
-        <img className={ `${style['img']}` } src={ discordImg } alt='discord-img'/>
-      </div>
-      <div className={ `${style['under-line']}` }></div>
+      <GroupServerButton first={ true }></GroupServerButton>
     </div>
    </div> 
   )
