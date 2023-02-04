b959d811dfc96666c95d28637d5b1c027fa4832c
jodie9596@gmail.com <jodie9596@gmail.com>
2023-02-04 20:48:34 +0900

    사이드바 아이콘 퍼블리싱

--- Front/discord-ssafy-dsg/src/components/leftSideBar/Index.js
+++ Front/discord-ssafy-dsg/src/components/leftSideBar/Index.js
@@ -1,9 +1,15 @@
-import style from './leftSideBar.module.css'
+import style from './leftSideBar.module.scss'
+import discordImg from '../../public/discord.png'
 
 function LeftSideBar() {
   return (
    <div className={ `${style['left-side-bar']}` }>
-
+    <div className={ `${style['first-area']}` }>
+      <div className={ `${style['img-area']}` }>
+        <img className={ `${style['img']}` } src={ discordImg } alt='discord-img'/>
+      </div>
+      <div className={ `${style['under-line']}` }></div>
+    </div>
    </div> 
   )
 }
